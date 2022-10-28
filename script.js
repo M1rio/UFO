c58328f79fbef1d6852485930e5ec214523d5f9e
Mario <107808343+M1rio@users.noreply.github.com>
2022-10-28 20:57:33 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+/* YAHOO Pure v0.4.2
+HTML5 & CSS3 layout  Example. 
+A layout example that shows off a blog page with a list of posts
+SOURCE: <a href="https://purecss.io/layouts/side-menu/" target="_blank"><i><b>HTML5 & CSS3 layout  Example</b></i></a>
+ by https://purecss.io/  
+ */
+
+(function (window, document) {
+
+    var layout   = document.getElementById('layout'),
+        menu     = document.getElementById('menu'),
+        menuLink = document.getElementById('menuLink');
+
+    function toggleClass(element, className) {
+        var classes = element.className.split(/\s+/),
+            length = classes.length,
+            i = 0;
+
+        for(; i < length; i++) {
+          if (classes[i] === className) {
+            classes.splice(i, 1);
+            break;
+          }
+        }
+        // The className is not found
+        if (length === classes.length) {
+            classes.push(className);
+        }
+
+        element.className = classes.join(' ');
+    }
+
+    menuLink.onclick = function (e) {
+        var active = 'active';
+
+        e.preventDefault();
+        toggleClass(layout, active);
+        toggleClass(menu, active);
+        toggleClass(menuLink, active);
+    };
+
+}(this, this.document));
